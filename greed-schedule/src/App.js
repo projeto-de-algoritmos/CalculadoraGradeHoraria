166a94eec745b7bef8399f67eb2e10129d605a41
Guilherme Barbosa <mrguilhermeferreira@gmail.com>
2023-01-16 22:43:37 -0300

    style: improves css

--- greed-schedule/src/App.js
+++ greed-schedule/src/App.js
@@ -1,22 +1,17 @@
 import SemesterConatiner from "./components/SemesterContainer";
 import "./App.css";
+import { ClassesSelector } from "./components/ClassesSelector";
+import { useState } from "react";
+import { greedySchedule } from "./utils/greedySchedule";
 
 function App() {
+  const [selectedClasses, setSelectedClasses] = useState([])
   return (
     <div className="App">
-      <button>Monte a minha grade</button>
-      <div className="generic-container">
-        <SemesterConatiner title={"1º semestre"}/>
-        <SemesterConatiner title={"2º semestre"}/>
-        <SemesterConatiner title={"3º semestre"}/>
-        <SemesterConatiner title={"4º semestre"}/>
-        <SemesterConatiner title={"5º semestre"}/>
-        <SemesterConatiner title={"6º semestre"}/>
-        <SemesterConatiner title={"7º semestre"}/>
-        <SemesterConatiner title={"8º semestre"}/>
-        <SemesterConatiner title={"9º semestre"}/>
-        <SemesterConatiner title={"10º semestre"}/>
-      </div>
+      <h2>Selecione as matérias que já cursou</h2>
+      <ClassesSelector onChange={(state) => setSelectedClasses(state)}/>
+      <button onClick={() => greedySchedule(selectedClasses)}>Monte a minha grade</button>
+      <SemesterConatiner title={"1º semestre"}/>
     </div>
   );
 }
